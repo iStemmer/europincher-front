90442bb9a3dd09a122eec1594420e5db65cef69f
Pavel Mikhalev <p.mikhalev@carepay.com>
2023-02-19 12:13:11 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'myAppVue',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
